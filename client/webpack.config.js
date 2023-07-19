60daec912db226f84f2ea6aa27c5efe74ec90926
jonny desktop <jon@uslms.com>
2023-07-18 18:54:43 -0600

    updated the webpack config and the service worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const { InjectManifest, GenerateSW } = require('workbox-webpack-plugin');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = function () {
   return {
@@ -13,22 +13,33 @@ module.exports = function () {
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'public'),
-      publicPath: '/',
     },    
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: "Webpack-Stuff"
+        title: "Just a text editor"
       }),
+      new WebpackPwaManifest({
+        name: 'My PWA',
+        inject: true,
+        short_name: 'JATE',
+        description: 'Just a text editor!',
+        background_color: '#ffffff',
+        start_url:'/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons")
+          }          
+        ]
+      }),
+      new GenerateSW(),
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'service-worker.js',
       }),
-      // new CopyWebpackPlugin({
-      //   patterns: [
-      //     { from: 'assets', to: 'assets' },
-      //   ],
-      // }),
     ],
     module: {
       rules: [
@@ -43,6 +54,7 @@ module.exports = function () {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
